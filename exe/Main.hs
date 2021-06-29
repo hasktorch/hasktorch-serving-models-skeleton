a6ebc842ef498c35bd14e7997a9d8b0fd38e19d4
Junji Hashimoto <junji.hashimoto@gmail.com>
2021-06-29 10:02:46 +0900

    Add boundingbox

--- exe/Main.hs
+++ exe/Main.hs
@@ -5,34 +5,44 @@
 
 module Main where
 
-import Control.Exception hiding (Handler)
 import Network.Wai.Handler.Warp (run)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad
 import Data.Aeson (FromJSON, ToJSON)
 import Data.ByteString.Lazy (toStrict, fromStrict)
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Lazy.Char8 as BLC
 import GHC.Generics (Generic)
 import Servant
-import Servant.Server
 import Servant.Multipart
 import Servant.JuicyPixels
 import Codec.Picture
 import Data.Text.Encoding (encodeUtf8)
-import Data.Maybe (isJust)
 import qualified Data.Text as T
-
+import Text.Read (readMaybe)
 
 import Torch 
+import Torch.Vision
 import System.Environment
 
-data Result = Result {
-  msg :: String, 
-  result :: [Float]
+data Result = Result
+  { msg :: String
+  , result :: [Float]
   } deriving (Show, Generic) 
 
 instance ToJSON Result
 instance FromJSON Result
+
+data Box = Box
+  { classid :: Int
+  , bx :: Float
+  , by :: Float
+  , bw :: Float
+  , bh :: Float
+  } deriving (Show) 
+
+newtype BoundingBox = BoundingBox [Box] deriving (Show)
+newtype Labels = Labels [String] deriving (Show)
 
 type HelloTorchAPI = "compute2x" :> Capture "value" Float :> Get '[JSON] [Result]
 
@@ -43,39 +53,92 @@ type API = HelloTorchAPI
 
 helloTorch :: MonadIO m => Float -> m [Result] 
 helloTorch value = pure $ 
-  [Result ("f(x) = 2.0 * x is " ++ show result ++ " for x = " ++ show value) [result]]
+  [Result ("f(x) = 2.0 * x is " ++ show result' ++ " for x = " ++ show value) [result']]
   where
     t = asTensor value :: Tensor
-    result = asValue (2.0 * t) :: Float
+    result' = asValue (2.0 * t) :: Float
+
+toLabels :: BL.ByteString -> Handler Labels
+toLabels str = do
+  let ssv :: [String]
+      ssv =
+        map BLC.unpack $
+        BL.split (fromIntegral $ fromEnum '\n') str
+  return $ Labels ssv
+
+
+toBoundingBox :: BL.ByteString -> Handler BoundingBox
+toBoundingBox str = do
+  let ssv =
+        filter (\v -> v /= []) $
+        map (BL.split (fromIntegral $ fromEnum ' ')) $
+        BL.split (fromIntegral $ fromEnum '\n') str
+  bbox <- forM ssv $ \v ->
+    case map BLC.unpack v of
+      (classid': x': y': w': h' :_) -> do
+        case (readMaybe classid', readMaybe x', readMaybe y', readMaybe w', readMaybe h') of
+          (Just cid, Just x, Just y, Just w, Just h) -> return $ Box cid x y w h
+          _ -> throwError $ err404 { errBody = fromStrict $ encodeUtf8 $ T.pack $ "1,Can not parse:" ++ show v}
+      _ -> throwError $ err404 { errBody = fromStrict $ encodeUtf8 $ T.pack $ "2,Can not parse:" ++ show ssv}
+  return $ BoundingBox bbox
+
+drawBoundingBox :: DynamicImage -> Labels -> BoundingBox -> Handler ()
+drawBoundingBox img (Labels labels) (BoundingBox bbox) = do
+  let input_image = convertRGB8 img
+      width = imageWidth input_image
+      height = imageHeight input_image
+  forM_ bbox $ \dat -> do
+    let cid = classid dat
+        x = fromIntegral width * bx dat
+        y = fromIntegral height * by dat
+        w = fromIntegral width * bw dat
+        h = fromIntegral height * bh dat
+        x0 = x - w/2
+        y0 = y - h/2
+        x1 = x + w/2
+        y1 = y + h/2
+    liftIO $ do
+      drawString (labels !! cid) (round x0+1) (round y0+1) (255,255,255) (0,0,0) input_image
+      drawRect (round x0) (round y0) (round x1) (round y1) (255,255,255) input_image
+  
 
 imageTorch :: MultipartData Mem -> Handler DynamicImage
 imageTorch multipartData = do
-  -- forM_ (inputs multipartData) $ \input -> do
-  imgs <- forM (files multipartData) $ \file -> do
-    if T.isSuffixOf "jpg" (fdFileName file) ||
-       T.isSuffixOf "png" (fdFileName file) ||
-       T.isSuffixOf "bmp" (fdFileName file) then do
-      liftIO $ putStrLn $ "Content of " ++ show (fdFileName file)
-      -- return (fdFileName file,fdPayload file)
-      case decodeImage (toStrict $ fdPayload file) of
-        Right img -> return (Just img)
-        Left err -> throwError $ err404 { errBody = fromStrict $ encodeUtf8 $ T.pack $ show err}
-    else
-      return Nothing
-  labels <- forM (files multipartData) $ \file -> do
-    if T.isSuffixOf "txt" (fdFileName file) ||
-       T.isSuffixOf "csv" (fdFileName file) then do
-      liftIO $ putStrLn $ "Content of " ++ show (fdFileName file)
-      -- return (fdFileName file,fdPayload file)
-      case decodeImage (toStrict $ fdPayload file) of
-        Right img -> return (Just img)
-        Left err -> throwError $ err404 { errBody = fromStrict $ encodeUtf8 $ T.pack $ show err}
-    else
-      return Nothing
-  case filter isJust imgs of
-    (Just file):_ -> return file
-    _ -> throwError $ err404 { errBody = "Not found an image file" }
+  img <-
+    case lookupFile "image" multipartData of
+      Right file -> do
+        case decodeImage (toStrict $ fdPayload file) of
+           Right img -> return img
+           Left err -> throwError $ err404 { errBody = fromStrict $ encodeUtf8 $ T.pack $ show err}
+      Left err -> throwError $ err404 { errBody = fromStrict $ encodeUtf8 $ T.pack $ show err}
 
+  Labels labels <- case lookupFile "labels" multipartData of
+    Right file -> toLabels (fdPayload file)
+    Left err -> throwError $ err404 { errBody = fromStrict $ encodeUtf8 $ T.pack $ show err}
+
+  BoundingBox bbox <- case lookupFile "bbox" multipartData of
+    Right file -> toBoundingBox (fdPayload file)
+    Left err -> throwError $ err404 { errBody = fromStrict $ encodeUtf8 $ T.pack $ show err}
+
+  let input_image = convertRGB8 img
+      width = imageWidth input_image
+      height = imageHeight input_image
+
+  forM_ bbox $ \dat -> liftIO $ do
+    let cid = classid dat
+        x = fromIntegral width * bx dat
+        y = fromIntegral height * by dat
+        w = fromIntegral width * bw dat
+        h = fromIntegral height * bh dat
+        x0 = x - w/2
+        y0 = y - h/2
+        x1 = x + w/2
+        y1 = y + h/2
+        label = if cid >= length labels then "Unknown" else labels !! cid
+    drawString label (round x0+1) (round y0+1) (255,255,255) (0,0,0) input_image
+    drawRect (round x0) (round y0) (round x1) (round y1) (255,255,255) input_image
+
+  return $ ImageRGB8 input_image
 
 torchApi :: Proxy API
 torchApi = Proxy
@@ -88,18 +151,12 @@ app = serve torchApi server
 
 main :: IO ()
 main = do
-  args <- getArgs
-  case args of
-    [] -> do
-      mport <- lookupEnv "PORT"
-      case mport of
-        Just port -> do
-          putStrLn $ "Running server on port " ++ port
-          run (read port) app
-        Nothing -> do
-          let port = 8081
-          putStrLn $ "Running server on port " ++ show port
-          run port app
-    port:_ -> do
+  mport <- lookupEnv "PORT"
+  case mport of
+    Just port -> do
       putStrLn $ "Running server on port " ++ port
       run (read port) app
+    Nothing -> do
+      let port = 8081
+      putStrLn $ "Running server on port " ++ show port
+      run port app
